549f34b0a288a6c8aefd3dc814e74c52c46f65c1
LightmareVII <48626681+LightmareVII@users.noreply.github.com>
2019-07-31 19:31:01 -0400

    futuremods.py
    Contains mods to add in the future
    multiline comment

---
+++ futureMods.py
@@ -0,0 +1,8 @@
+"""
+Modules that I want to add in the future
+
+Add information for class/race to apply their bonuses as well
+Include proficiency booleans
+Calculate Proficiencies, saving throws, and passive perception
+Save character info into .csv to call for later use and auto leveling
+"""
