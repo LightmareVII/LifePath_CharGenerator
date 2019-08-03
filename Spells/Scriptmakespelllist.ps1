5e1a9f672cdd91c7ef2bc4c60ee41d993822db0f
LightmareVII <48626681+LightmareVII@users.noreply.github.com>
2019-08-02 23:07:35 -0400

    List of Spells
    Got a list of spells and made script to parse for a python list.
    Cantrips and Level 1 only ones that need to be implemented now

---
+++ Spells/Scriptmakespelllist.ps1
@@ -0,0 +1,26 @@
+﻿$csv = Import-Csv "D:\Python Projects\LifePath_CharGenerator\Spells\FullList.csv"
+
+$spells = @{"Cantrip" = @(); "Level01" = @(); "Level02" = @(); "Level03" = @(); "Level04" = @(); "Level05" = @(); "Level06" = @(); "Level07" = @(); "Level08" = @(); "Level09" = @(); "Level10" = @()}
+
+foreach ($i in $csv){
+    #Write-Host $i.Name
+    #$i.Level
+    switch ($i.Level.Trim('')){
+    "Cantrip" {$spells.Cantrip += $i.name; break}
+    "10th" {$spells.Level10 += $i.name; break}
+    "2nd" {$spells.Level02 += $i.name; break}
+    "3rd" {$spells.Level03 += $i.name; break}
+    "4th" {$spells.Level04 += $i.name; break}
+    "5th" {$spells.Level05 += $i.name; break}
+    "6th" {$spells.Level06 += $i.name; break}
+    "7th" {$spells.Level07 += $i.name; break}
+    "8th" {$spells.Level08 += $i.name; break}
+    "9th" {$spells.Level09 += $i.name; break}
+    "1st" {$spells.Level01 += $i.name; break}
+    default {write-host $i.Name Failed}
+    }
+}
+
+foreach ($i in $spells.Level01){
+    Write-Host "'$i',"
+}
